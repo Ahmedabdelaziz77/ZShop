1b2defa1890a83ccde93ab8085be45832f2a7c31
Ahmedabdelaziz77 <zozahmed770@gmail.com>
2025-09-07 15:54:18 +0300

    first commit

---
+++ apps/auth-service/src/main.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello API" });
+});
+
+const port = process.env.PORT || 6001;
+const server = app.listen(port, () => {
+  console.log(`Auth service listening at http://localhost:${port}/api`);
+});
+server.on("error", (err) => {
+  console.log("Server Error :", err);
+});
